cba8daf85d727f6ed8b5d0ce9c87146975dd6024
devIggy <devdevgrin@gmail.com>
2024-03-22 13:19:50 +0300

    feat: hamburger, menu and cross js & animation, page navigation

---
+++ dist/js/script.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelector(".menu"),
+      menuLink = document.querySelectorAll(".menu__link"),
+      hamburger = document.querySelector(".hamburger");
+    //   cross = document.querySelector(".cross");
+  
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("menu_active");
+    //   cross.classList.toggle("cross_active");
+    });
+
+    // cross.addEventListener("click", () => {
+    //     hamburger.classList.toggle("hamburger_active");
+    //     menu.classList.toggle("menu_active");
+    //     cross.classList.toggle("cross_active");
+    //   });
+  
+    menuLink.forEach((item) => {
+      item.addEventListener("click", () => {
+        hamburger.classList.toggle("hamburger_active");
+        menu.classList.toggle("menu_active");
+        cross.classList.toggle("cross_active");
+      });
+    });
+  });
