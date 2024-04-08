1c619590f64df2830dea0aa560c27bb088f03e7e
devIggy <devdevgrin@gmail.com>
2024-04-08 11:11:16 +0300

    fix: classes names and flex layout

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,34 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const menu = document.querySelector(".menu"),
-      menuLink = document.querySelectorAll(".menu__link"),
-      hamburger = document.querySelector(".hamburger");
-    //   cross = document.querySelector(".cross");
-  
-    hamburger.addEventListener("click", () => {
-      hamburger.classList.toggle("hamburger_active");
-      menu.classList.toggle("menu_active");
-    //   cross.classList.toggle("cross_active");
-    });
+  const menu = document.querySelector(".menu"),
+    menuLink = document.querySelectorAll(".menu__link"),
+    hamburger = document.querySelector(".hamburger");
+  //   cross = document.querySelector(".cross");
 
-    // cross.addEventListener("click", () => {
-    //     hamburger.classList.toggle("hamburger_active");
-    //     menu.classList.toggle("menu_active");
-    //     cross.classList.toggle("cross_active");
-    //   });
-  
-    menuLink.forEach((item) => {
-      item.addEventListener("click", () => {
-        hamburger.classList.remove("hamburger_active");
-        menu.classList.remove("menu_active");
-      });
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    menu.classList.toggle("menu_active");
+    //   cross.classList.toggle("cross_active");
+  });
+
+  // cross.addEventListener("click", () => {
+  //     hamburger.classList.toggle("hamburger_active");
+  //     menu.classList.toggle("menu_active");
+  //     cross.classList.toggle("cross_active");
+  //   });
+
+  menuLink.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.remove("hamburger_active");
+      menu.classList.remove("menu_active");
     });
   });
+});
+
+
+//пересчет процентов progress__items
+const percents = document.querySelectorAll(".progress__item-percents"),
+  lines = document.querySelectorAll(".progress__item__value span");
+
+percents.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
